7d305d80fa6d425a6db80bb0018aee7051044554
nboari <nboari@localhost.localdomain>
2018-02-25 23:30:41 -0300

    testing

--- cliente.py
+++ cliente.py
@@ -14,15 +14,27 @@ def index():
     return "GET: <a href='" + get + "'>"+ get + "</a>"
 
 @app.route('/gimnasio/api/v1.0/clientes', methods=['GET'])
-def get_tasks():
+def get_all():
     con = sqlite3.connect("gimnasio.db")
     con.row_factory = sqlite3.Row # This enables column access by name: row['column_name']
     cur = con.cursor()
     cur.execute("select * from cliente")
+    rows = cur.fetchall()
+    dic = [dict(ix) for ix in rows]
+    resp = json.dumps(dic)
+    con.close()
+    return '{ "clientes": ' + resp + '}'
+
+@app.route('/gimnasio/api/v1.0/clientes/1', methods=['GET'])
+def get_one():
+    con = sqlite3.connect("gimnasio.db")
+    con.row_factory = sqlite3.Row # This enables column access by name: row['column_name']
+    cur = con.cursor()
+    cur.execute("select * from cliente where id=1")
     rows = cur.fetchall()
     resp = json.dumps([dict(ix) for ix in rows])
     con.close()
     return '{ "clientes": ' + resp + '}'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
